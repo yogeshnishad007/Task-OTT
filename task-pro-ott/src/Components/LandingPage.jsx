dd49317b327ce45a18469d28aaa02d9d16130bf3
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-05-19 23:58:54 +0530

    side bar

--- task-pro-ott/src/Components/LandingPage.jsx
+++ task-pro-ott/src/Components/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import SideBar from './SideBar'
 
 
 const LandingPage = () => {
   return (
     <div>
         <Navbar/>
+        <SideBar/>
         
     </div>
   )
--- task-pro-ott/src/Components/SideBar.jsx
+++ task-pro-ott/src/Components/SideBar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import "../style.css"
 const SideBar = () => {
   return (
-    <div>
+    <div className='side-bar-con'>
 
-           
+                <h2>
+                    Channels   
+               </h2>
+
+               <h2>
+                    Languages
+               </h2>
+
+               <h2>
+                    Geners
+               </h2>
     </div>
   )
 }
